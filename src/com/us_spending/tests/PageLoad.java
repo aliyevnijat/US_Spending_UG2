09d5be4e9fa9b06433e4a01e4ae5e2287151de2c
aliyevnijat <aliyevnicat@gmail.com>
2018-03-13 18:56:15 -0400

    Added for loop.

--- src/com/us_spending/tests/PageLoad.java
+++ src/com/us_spending/tests/PageLoad.java
@@ -12,5 +12,9 @@ public class PageLoad extends TestBaseClass {
 		//Ekaterina was here
 		//Maria
 		//==================//
+		int[] arr = { 1, 4, 56, 78, 65 };
+		for (int i = 0; i < arr.length; i++) {
+			System.out.println("Numbers " + arr[i]);
+		}
 	}
 }
